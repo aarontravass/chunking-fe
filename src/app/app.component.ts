0416d40ec73ba6d53385a15c0a4d7b85cfdfa5d9
aarontravass <aaron.travass@hotmail.com>
2023-05-05 16:16:14 -0400

    added chunks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,21 +9,34 @@ import { AppService } from './app.service';
 export class AppComponent {
     title = 'chunking-fe';
     file = new Blob();
+    private readonly chunk_size = 30;
     constructor(private readonly appService: AppService) {}
     onChange(event: any) {
         this.file = event.target.files[0];
+        console.log(this.file)
     }
     async submit() {
-        const form = new FormData();
-        form.append('file', this.file);
-        await this.appService
-            .send(form)
+        
+        const buff = await this.file.arrayBuffer();
+        console.log(buff)
+        let header_start = 1, header_end = header_start + this.chunk_size - 1, header_total = buff.byteLength;
+        while(header_end<header_total){
+            const data = {
+                data: buff.slice(header_start, header_end)
+            }
+            await this.appService
+            .send(data, header_start, header_end, header_total)
             .toPromise()
             .then((res) => {
                 console.log(res);
+                header_start = header_end + 1;
+                header_end = header_end + this.chunk_size;
             })
             .catch((er) => {
                 console.error(er);
             });
+            
+        }
+        
     }
 }
--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpParams } from "@angular/common/http";
+import { HttpClient, HttpHeaders, HttpParams } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
 import { API_URL } from "./globals";
@@ -12,8 +12,12 @@ export class AppService {
     constructor(private readonly http: HttpClient) { }
 
 
-    send(form: FormData): Observable<any> {
-        return this.http.post<any>(API_URL + 'api/v1/send', form);
+    send(data:any, header_start: number, header_end:number, header_total:number): Observable<any> {
+        const headers= new HttpHeaders()
+                            .set('header_start', header_start.toString())
+                            .set('header_end', header_end.toString())
+                            .set('header_total', header_total.toString())
+        return this.http.post<any>(API_URL + 'api/v1/send', {headers});
            
     }
 
